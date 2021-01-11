b736d4fbd8c75cf197c62fb80780c2fb0ca510b7
Berney Alec <beral@sevjnet.ch>
2021-01-11 15:17:07 +0100

    update script datas

--- DB_DATA.sql
+++ DB_DATA.sql
@@ -66,8 +66,13 @@ VALUES
 ( 'Bosch', 'Marc', 'Stake@giantsgaming.com', 'Stake', '2000-07-20' ),
 ( 'Cortés', 'Samuel', 'Zamué@giantsgaming.com', 'Zamué', '2000-10-19' ),
 ( 'Benayachi', 'Amine', 'itachi@giantsgaming.com', 'itachi', '2003-08-13' ),
-( 'John', 'Doe', 'johnny@unc.com', 'johnny', '2000-01-01' ),
-( 'Pierre', 'Kiroule', 'pierre@sansequipe.com', 'pierre', '1990-01-01' );
+( 'Doe', 'John', 'johnny@uc1.com', 'johnny', '2000-01-01' ),
+( 'Kiroule', 'Pierre', 'pierre@uc2.com', 'pierre', '1990-02-01' ),
+( 'Kévin', 'Jean', 'kevin@uc2.com', 'kévin', '1990-03-01' ),
+( 'Gabin', 'Jean', 'gabin@sansequipe.com', 'gabin', '1990-04-01' ),
+( 'Berset', 'Alin', 'theking@sansequipe.com', 'theking', '1980-04-01' ),
+( 'Maxime', 'Berthoud', 'max@sansequipe.com', 'max', '2005-04-01' );
+
 
 INSERT INTO Equipe
 VALUES 
@@ -81,7 +86,8 @@ VALUES
 ( 'FCB', 'FC Barcelona', 22 ),
 ( 'GAR', 'Galaxy Racer', 25 ),
 ( 'GIA', 'Giants Gaming', 28 ),
-( 'UNC', 'Uncomplete',  31);
+( 'UC1', 'Uncomplete1',  31),
+( 'UC2', 'Uncomplete2',  33);
 
 
 INSERT INTO Equipe_Joueur
@@ -89,6 +95,9 @@ VALUES
 ( 'SLY', 1,  '2014-01-01 00:00:00','2018-03-01 00:00:00' ),
 ( 'SLY', 2,  '2015-01-01 00:00:00','2018-04-01 00:00:00' ),
 ( 'SLY', 3,  '2016-01-01 00:00:00','2018-02-01 00:00:00' ),
+
+('ROC', 34, '0001-01-01 00:00:00', NULL ),
+('SLY', 35, '0001-01-01 00:00:00', NULL ),
 
 ( 'ROC', 1,  '2020-01-01 00:00:00', NULL ),
 ( 'ROC', 2,  '2020-01-01 00:00:00', NULL ),
@@ -102,14 +111,14 @@ VALUES
 ( 'TL', 10,  '2020-01-01 00:00:00', NULL ),
 ( 'TL', 11,  '2020-01-01 00:00:00', NULL ),
 ( 'TL', 12,  '2020-01-01 00:00:00', NULL ),
-( 'EE', 13,  '2020-01-01 00:00:00', '2020-12-28 17:00:00' ),
+( 'EE', 13,  '2020-01-01 00:00:00', NULL ),
 ( 'EE', 14,  '2020-01-01 00:00:00', NULL ),
-( 'EE', 15,  '2020-01-01 00:00:00', NULL ),
-( 'VIT', 16,  '2020-01-01 00:00:00', '2020-12-27 20:00:00' ),
-( 'VIT', 17,  '2020-01-01 00:00:00', NULL ),
+( 'EE', 15,  '2020-01-01 00:00:00', '2020-12-28 17:00:00' ),
+( 'VIT', 16,  '2020-01-01 00:00:00', NULL ),
+( 'VIT', 17,  '2020-01-01 00:00:00', '2020-12-27 20:00:00' ),
 ( 'VIT', 18,  '2020-01-01 00:00:00', NULL ),
-( 'DIG', 19,  '2020-01-01 00:00:00', '2020-12-26 19:00:00' ),
-( 'DIG', 20,  '2020-01-01 00:00:00', NULL ),
+( 'DIG', 19,  '2020-01-01 00:00:00', NULL ),
+( 'DIG', 20,  '2020-01-01 00:00:00', '2020-12-26 19:00:00' ),
 ( 'DIG', 21,  '2020-01-01 00:00:00', NULL ),
 ( 'FCB', 22,  '2020-01-01 00:00:00', NULL ),
 ( 'FCB', 23,  '2020-01-01 00:00:00', NULL ),
@@ -121,19 +130,29 @@ VALUES
 ( 'GIA', 29,  '2020-01-01 00:00:00', NULL ),
 ( 'GIA', 30,  '2020-01-01 00:00:00', NULL ),
 
-( 'VIT', 19,  '2020-12-28 00:00:00', NULL ),
-( 'DIG', 16,  '2020-12-29 00:00:00', NULL ),
+( 'VIT', 20,  '2020-12-28 00:00:00', NULL ),
+( 'DIG', 17,  '2020-12-29 00:00:00', NULL ),
 
 ( 'EE', 5,  '2020-12-30 00:00:00', NULL ),
-( 'LSE', 15,  '2020-12-30 00:00:00', NULL );
+( 'LSE', 15,  '2020-12-30 00:00:00', NULL ),
+
+( 'UC1', 31,  '2020-01-30 00:00:00', NULL ),
+( 'UC1', 32,  '2020-01-30 00:00:00', NULL ),
+( 'UC2', 33,  '2020-01-30 00:00:00', NULL ),
+( 'UC2', 36,  '2020-01-30 00:00:00', '2021-01-01 20:00:00' );
 
 
 INSERT INTO Tournoi (dateHeureDebut, nom, nbEquipesMax, dateHeureFin)
 VALUES 
 ( '2020-02-01 20:00:00', 'Le Franco-Suisse', 4, '2020-02-01 23:00:00' ),
 ( '2020-03-01 20:00:00', 'IEM', 8, '2020-04-01 23:00:00' ),
-( NOW(), 'Tournoi de préparation en cours', 2, NULL ),
-( NOW(), 'Tournoi de préparation annulé', 2, NULL );
+( '2020-12-02', 'Tournoi de préparation en cours', 2, NULL ),
+( '2020-12-01', 'Tournoi de préparation annulé', 2, '2020-12-01' ),
+( '2020-12-03', 'Tournoi 4 équipe en cours séries en cours', 4, NULL ),
+( '2020-12-04', 'Tournoi 4 équipe en cours tour 1 terminé', 4, NULL ),
+( '2022-02-01', 'Tournoi 4 équipe en attente 3/4', 4, NULL ),
+( '2022-03-01', 'Tournoi 4 équipe en attente 4/4', 4, NULL ),
+( Now(), 'Tournoi 4 équipe en attente prêt 4/4', 4, NULL );
 
 
 INSERT INTO Tour
@@ -147,7 +166,22 @@ VALUES
 
 ( 1, 3, 3 ),
 
-( 1, 3, 4 );
+( 1, 3, 4 ),
+
+( 2, 3, 5 ),
+( 1, 3, 5 ),
+
+( 2, 1, 6 ),
+( 1, 3, 6 ),
+
+( 2, 1, 7 ),
+( 1, 1, 7 ),
+
+( 2, 1, 8 ),
+( 1, 1, 8 ),
+
+( 2, 1, 9 ),
+( 1, 1, 9 );
 
 INSERT INTO Tournoi_Equipe
 VALUES 
@@ -168,9 +202,34 @@ VALUES
 ( 3, 'ROC', '2020-01-12 20-00-00' ),
 ( 3, 'SLY', '2020-02-12 20-01-00' ),
 
-( 4, 'ROC', '2020-10-12 20-00-00' );
+( 4, 'ROC', '2020-10-12 20-00-00' ),
+
+( 5, 'GAR', '2020-11-05 20-04-00' ),
+( 5, 'LSE', '2020-11-06 20-05-00' ),
+( 5, 'GIA', '2020-11-07 20-05-00' ),
+( 5, 'SLY', '2020-11-08 20-06-00' ),
+
+( 6, 'ROC', '2020-09-01 20-00-00' ),
+( 6, 'SLY', '2020-10-02 20-01-00' ),
+( 6, 'LSE', '2020-11-03 20-02-00' ),
+( 6, 'VIT', '2020-12-01 20-02-00' ),
+
+( 7, 'GIA', '2020-09-01 20-00-00' ),
+( 7, 'EE', '2020-10-02 20-01-00' ),
+( 7, 'LSE', '2020-11-03 20-02-00' ),
+
+( 8, 'ROC', '2020-09-01 20-00-00' ),
+( 8, 'SLY', '2020-10-02 20-01-00' ),
+( 8, 'LSE', '2020-11-03 20-02-00' ),
+( 8, 'VIT', '2020-12-01 20-02-00' ),
+
+( 9, 'TL', '2020-08-01 20-00-00' ),
+( 9, 'FCB', '2020-09-02 20-01-00' ),
+( 9, 'GAR', '2020-09-03 20-02-00' ),
+( 9, 'GIA', '2020-10-01 20-02-00' );
 
 
+-- Tournoi 1
 INSERT INTO Serie
 VALUES ( 1, 2, 1, 'ROC', 'SLY' );
 
@@ -178,7 +237,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 1, 2, 1 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 1, 3, 1, 1, 1, 2, 1 ),
 ( 2, 0, 3, 1, 1, 2, 1 ),
@@ -194,7 +253,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 2, 2, 1 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 13, 3, 1, 1, 2, 2, 1 ),
 ( 14, 0, 3, 1, 2, 2, 1 ),
@@ -212,7 +271,7 @@ VALUES
 ( 2, 1, 1, 1 ),
 ( 3, 1, 1, 1 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 1, 1, 5, 1, 1, 1, 1 ),
 ( 2, 0, 3, 1, 1, 1, 1 ),
@@ -235,6 +294,8 @@ VALUES
 ( 14, 0, 1, 3, 1, 1, 1 ),
 ( 15, 1, 4, 3, 1, 1, 1 );
 
+
+-- Tournoi 2
 INSERT INTO Serie
 VALUES ( 1, 3, 2, 'ROC', 'TL');
 
@@ -242,7 +303,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 1, 3, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 1, 0, 5, 1, 1, 3, 2 ),
 ( 2, 3, 1, 1, 1, 3, 2 ),
@@ -258,7 +319,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 2, 3, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 13, 2, 8, 1, 2, 3, 2 ),
 ( 14, 3, 2, 1, 2, 3, 2 ),
@@ -274,7 +335,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 3, 3, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 19, 0, 0, 1, 3, 3, 2 ),
 ( 20, 0, 3, 1, 3, 3, 2 ),
@@ -290,7 +351,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 4, 3, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 22, 3, 1, 1, 4, 3, 2 ),
 ( 23, 0, 4, 1, 4, 3, 2 ),
@@ -306,7 +367,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 1, 2, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 1, 3, 4, 1, 1, 2, 2 ),
 ( 2, 2, 0, 1, 1, 2, 2 ),
@@ -322,7 +383,7 @@ INSERT INTO `Match`
 VALUES 
 ( 1, 2, 2, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 4, 3, 2, 1, 2, 2, 2 ),
 ( 5, 0, 7, 1, 2, 2, 2 ),
@@ -339,7 +400,7 @@ VALUES
 ( 1, 1, 1, 2 ),
 ( 2, 1, 1, 2 );
 
-INSERT INTO Match_Joueur (idJoueur, nbButs, nbArrets, idMatch, idSerie, noTour, idTournoi)
+INSERT INTO Match_Joueur
 VALUES 
 ( 1, 1, 3, 1, 1, 1, 2 ),
 ( 2, 1, 4, 1, 1, 1, 2 ),
@@ -354,6 +415,8 @@ VALUES
 ( 5, 1, 3, 2, 1, 1, 2 ),
 ( 6, 1, 6, 2, 1, 1, 2 );
 
+
+-- Tournoi 3
 INSERT INTO Serie
 VALUES ( 1, 1, 3, 'ROC', 'SLY' );
 
@@ -369,3 +432,116 @@ VALUES
 ( 7, 2, 0, 1, 1, 1, 3 ),
 ( 8, 1, 1, 1, 1, 1, 3 ),
 ( 9, 1, 5, 1, 1, 1, 3 );
+
+
+-- Tournoi 4
+INSERT INTO Serie
+VALUES ( 1, 1, 4, NULL, NULL );
+
+
+-- Tournoi 5
+INSERT INTO Serie
+VALUES ( 1, 2, 5, 'GAR', 'LSE');
+
+INSERT INTO `Match`
+VALUES 
+( 1, 1, 2, 5 );
+
+INSERT INTO Match_Joueur
+VALUES 
+( 4, 1, 5, 1, 1, 2, 5 ),
+( 5, 3, 1, 1, 1, 2, 5 ),
+( 6, 2, 2, 1, 1, 2, 5 ),
+( 25, 3, 1, 1, 1, 2, 5 ),
+( 26, 1, 3, 1, 1, 2, 5 ),
+( 27, 2, 0, 1, 1, 2, 5 );
+
+INSERT INTO Serie
+VALUES ( 2, 2, 5, 'GIA', 'SLY' );
+
+INSERT INTO `Match`
+VALUES 
+( 1, 2, 2, 5 ),
+( 2, 2, 2, 5 );
+
+INSERT INTO Match_Joueur
+VALUES 
+( 7, 2, 8, 1, 2, 2, 5 ),
+( 8, 3, 2, 1, 2, 2, 5 ),
+( 9, 1, 0, 1, 2, 2, 5 ),
+( 28, 1, 4, 1, 2, 2, 5 ),
+( 29, 0, 1, 1, 2, 2, 5 ),
+( 30, 1, 3, 1, 2, 2, 5 );
+
+INSERT INTO Match_Joueur
+VALUES 
+( 7, 1, 3, 2, 2, 2, 5 ),
+( 8, 0, 2, 2, 2, 2, 5 ),
+( 9, 2, 0, 2, 2, 2, 5 ),
+( 28, 3, 5, 2, 2, 2, 5 ),
+( 29, 2, 2, 2, 2, 2, 5 ),
+( 30, 0, 3, 2, 2, 2, 5 );
+
+INSERT INTO Serie
+VALUES ( 1, 1, 5, NULL, NULL );
+
+
+-- Tournoi 6
+INSERT INTO Serie
+VALUES ( 1, 2, 6, 'ROC', 'SLY' );
+
+INSERT INTO `Match`
+VALUES 
+( 1, 1, 2, 6 );
+
+INSERT INTO Match_Joueur
+VALUES 
+( 1, 6, 1, 1, 1, 2, 6 ),
+( 2, 0, 6, 1, 1, 2, 6 ),
+( 3, 2, 5, 1, 1, 2, 6 ),
+( 7, 2, 2, 1, 1, 2, 6 ),
+( 8, 1, 3, 1, 1, 2, 6 ),
+( 9, 1, 1, 1, 1, 2, 6 );
+
+INSERT INTO Serie
+VALUES ( 2, 2, 6, 'LSE', 'VIT' );
+
+INSERT INTO `Match`
+VALUES 
+( 1, 2, 2, 6 );
+
+INSERT INTO Match_Joueur
+VALUES 
+( 4, 2, 1, 1, 2, 2, 6 ),
+( 5, 0, 3, 1, 2, 2, 6 ),
+( 6, 1, 5, 1, 2, 2, 6 ),
+( 16, 0, 2, 1, 2, 2, 6 ),
+( 17, 0, 1, 1, 2, 2, 6 ),
+( 18, 1, 4, 1, 2, 2, 6 );
+
+INSERT INTO Serie
+VALUES ( 1, 1, 6, NULL, NULL );
+
+
+-- Tournoi 7
+INSERT INTO Serie
+VALUES 
+( 1, 2, 7, NULL, NULL ),
+( 2, 2, 7, NULL, NULL ),
+( 1, 1, 7, NULL, NULL );
+
+
+-- Tournoi 8
+INSERT INTO Serie
+VALUES 
+( 1, 2, 8, NULL, NULL ),
+( 2, 2, 8, NULL, NULL ),
+( 1, 1, 8, NULL, NULL );
+
+
+-- Tournoi 9
+INSERT INTO Serie
+VALUES 
+( 1, 2, 9, NULL, NULL ),
+( 2, 2, 9, NULL, NULL ),
+( 1, 1, 9, NULL, NULL );
